c1c35992aeab381902a43b7a38763418fc26b8b5
MD Anik <122194751+sranik28@users.noreply.github.com>
2024-01-13 20:01:51 +0600

    allContact update

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ async function run() {
       res.send(result);
     });
 
+
+    
     app.get("/allContact/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
