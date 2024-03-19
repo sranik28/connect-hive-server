20691c37c6410044b16529c838d141e3de5b8140
MD Anik <122194751+sranik28@users.noreply.github.com>
2024-03-19 19:45:47 +0600

    new added

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ async function run() {
       console.log(result);
     });
 
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
